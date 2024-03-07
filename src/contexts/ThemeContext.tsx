034feea8332cd77c934a4255fcaecb18c00a7dca
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-03-06 21:26:21 -0300

    theme context added

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,36 @@
+import { ReactNode, createContext, useContext, useState } from "react"
+
+type Theme = typeof light
+
+import light from "../themes/light"
+import dark from "../themes/dark"
+import darker from "../themes/darker"
+
+const themes = [light, dark, darker]
+
+interface IThemeContext {
+  theme: Theme
+  toggleTheme: (themeTitle: "light" | "dark" | "darker") => void
+}
+
+const ThemeContext = createContext<IThemeContext>({} as IThemeContext)
+
+interface ThemeContextProviderProps {
+  children: ReactNode
+}
+
+export function ThemeContextProvider({ children }: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState(light)
+
+  function toggleTheme(themeTitle: "light" | "dark" | "darker") {
+    setTheme(themes.find(t => t.title === themeTitle) as Theme)
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext)

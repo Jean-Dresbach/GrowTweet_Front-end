1612486f1a3ffa8cc773aa3dc4dad03d870910ca
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-03-09 21:12:46 -0300

    theme context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { ThemeProvider } from "styled-components"
+
 import Routes from "./routes/Router"
 import GlobalStyles from "./styles/global"
+import { useTheme } from "./contexts/ThemeContext"
 
 export function App() {
+  const { theme } = useTheme()
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Routes />
-    </>
+    </ThemeProvider>
   )
 }

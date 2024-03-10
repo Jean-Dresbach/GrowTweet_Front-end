07146b81121aba6f7bc1a9af23cbd8aad11168ba
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-03-09 21:13:36 -0300

    input component created

---
+++ src/pages/LoginAndSignup/components/Input/index.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent, useEffect, useState } from "react"
+
+import { Wrapper } from "./styles"
+
+interface InputProps {
+  placeholder: string
+  name: string
+  value: string
+  isValid: boolean
+  errorMessage: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+export function Input({
+  placeholder,
+  name,
+  onChange,
+  value,
+  errorMessage,
+  isValid
+}: InputProps) {
+  const [inputClass, setInputClass] = useState("")
+
+  useEffect(() => {
+    setInputClass(isValid ? "" : "invalid")
+  }, [isValid])
+
+  return (
+    <Wrapper>
+      <input
+        key={name}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        type="text"
+        placeholder=" "
+        className={inputClass}
+      />
+      <label htmlFor={name}>{placeholder}</label>
+      {!isValid && <span>{errorMessage}</span>}
+    </Wrapper>
+  )
+}
---
+++ src/pages/LoginAndSignup/components/Input/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+  position: relative;
+  padding-top: 13px;
+  width: 100%;
+
+  input {
+    width: 100%;
+
+    outline: none;
+    margin-bottom: 24px;
+    border-radius: 5px;
+    color: ${({ theme }) => theme.colors.quinary};
+    border: 1px solid ${({ theme }) => theme.colors.quinary};
+    background-color: transparent;
+    padding: 16px;
+
+    transition: all 0.1s linear;
+  }
+
+  input:focus {
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+  }
+
+  label {
+    position: absolute;
+    top: calc(28%);
+    left: 15px;
+
+    color: ${({ theme }) => theme.colors.quinary};
+
+    background-color: ${({ theme }) => theme.colors.secondary};
+    padding: 5px;
+
+    transition: all 0.1s linear;
+  }
+
+  input:focus + label,
+  input:not(:placeholder-shown) + label {
+    font-size: 13px;
+    top: 0;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+
+  input:not(:placeholder-shown) {
+    border-color: ${({ theme }) => theme.colors.primary};
+  }
+
+  span {
+    position: absolute;
+    left: 65px;
+    bottom: -15px;
+
+    font-size: 13px;
+    color: tomato !important;
+  }
+`
